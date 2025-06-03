c46bfc1798e72e4da3d3a366000347788a6faf8a
zer0anomaly <zeroanomale@gmail.com>
2025-06-03 13:58:29 +0500

    New One

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ let quote = document.getElementById('quote');
 let author = document.getElementById('author');
 
 quotebutton.addEventListener('click', () => {
-	fetch("https://favqs.com/api/qotd")
+	fetch("https://zenquotes.io/api/random")
 		.then(response => response.json())
 		.then(data => {
-			quote.textContent = data.quote.body
-			author.textContent = data.quote.author
+			quote.textContent = data[0].q
+			author.textContent = data[0].a
+		})
+		.catch(error => {
+			console.error('Error fetching quote:', error);
+      		quote.textContent = 'Oops! Could not fetch a quote right now.';
+      		author.textContent = '';
 		})
 });
